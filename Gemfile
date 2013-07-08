845a8fe68603abed37b740a30ef705b3a47ceb5c
Ignacy Moryc <imoryc@gmail.com>
2013-07-08 17:18:22 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'rspec-rails', '~> 2.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarks::Application.config.secret_token = '8c20ecf024e1108f00a28dbe9f906eb5b6ea7998ff37c515fcbf9b7ddac0669cdf8bef4bef39de2b929c5c1659974048a3b121d146cf4472e62767bae8bcbc01'
