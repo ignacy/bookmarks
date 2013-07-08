181cca69fbf88320d6a7cf4ad8e240b40a64deb3
Ignacy Moryc <imoryc@gmail.com>
2013-07-08 18:26:31 +0200

    Adding bookmarks without tags, and validations for bookarm without URL

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,5 @@
 class Bookmark < ActiveRecord::Base
+  validates :url, presence: true
   attr_accessible :shortening, :site_id, :url, :tag_list
   acts_as_taggable
 end
--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -9,4 +9,18 @@ describe "Adding new bookmark", type: :feature do
       click_button 'Create Bookmark'
     end.to change { Bookmark.count }.by(1)
   end
+
+  it "works with URL only (no tags)" do
+    expect do
+      visit '/bookmarks/new'
+      fill_in 'URL', with: 'http://example.com'
+      click_button 'Create Bookmark'
+    end.to change { Bookmark.count }.by(1)
+  end
+
+  it "does not work without URL" do
+    visit '/bookmarks/new'
+    click_button 'Create Bookmark'
+    expect(page).to have_content "Url can't be blank"
+  end
 end
